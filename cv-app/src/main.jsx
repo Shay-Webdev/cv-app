2a20fe78577a9e7803840242b1b4fc78dad0eff1
shay-Webdev <181019353+Shay-Webdev@users.noreply.github.com>
2025-01-05 13:56:23 +0530

    Finish build

--- cv-app/src/main.jsx
+++ cv-app/src/main.jsx
@@ -3,7 +3,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { createRoot } from 'react-dom/client';
 import './styles/index.css';
-import App from './components/App.jsx';
 import PageContainer from './components/container.jsx';
 
 createRoot(document.getElementById('root')).render(
